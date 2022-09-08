e729dba5046076cd628272bb17ad141580ba848b
LandonHolt94 <111383436+LandonHolt94@users.noreply.github.com>
2022-09-07 22:17:48 -0700

    Activity 2

---
+++ app/src/main/java/com/example/scrollingactivity2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.scrollingactivity2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webview;
+    private String url = "https://www.valleycollege.edu";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview = (WebView)findViewById(R.id.webview);
+        webview.loadUrl(url);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
+
+    }
+}
